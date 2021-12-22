cfcfd54f20c4efa41ed0d95b7ca5fa4c3635bd45
Sabina <alievasabi28@gmail.com>
2021-12-22 20:45:22 +0300

    Java task

--- Ex1_Lab4.java
+++ Ex1_Lab4.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 public class Ex1_Lab4 {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите массив: ");
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        String input = "";
         for (int i = 0; ; i++) {
             String t = sc.next();
             if (t.equals("quit")) {
@@ -12,25 +11,31 @@ public class Ex1_Lab4 {
             }
             else {
                 try {
-                    arr.add(Integer.parseInt(t));
+                    int exam = Integer.parseInt(t);
+                    input += t;
                 }
                 catch (Exception e) {
                     System.out.println("Введено не число!");
                 }
             }
         }
-        double [] arr2 = new double[arr.size()];
-        double sum = arr.get(0);
-        arr2[0] = arr.get(0);
-        for (int i = 1; i < arr.size(); i++) {
-           arr2[i] = sum/((double)i);
-           sum += arr.get(i);
+        int [] arr = new int[input.length()];
+        String [] mass = input.split("");
+        for (int i = 0; i<input.length(); i++) {
+            arr[i] = Integer.parseInt(mass[i]);
         }
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.print(arr.get(i) + " ");
+        double [] arr2 = new double[arr.length];
+        double sum = arr[0];
+        arr2[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+           arr2[i] = sum/((double)i);
+           sum += arr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr2[i] + " ");
         }
         sc.close();
